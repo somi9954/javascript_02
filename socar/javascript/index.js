42fcd6e46c2b9b39f7bbf05b343f8f8f63446860
somi9954 <jolh999@gmail.com>
2025-03-21 19:00:17 +0900

    socar

---
+++ socar/javascript/index.js
@@ -0,0 +1,30 @@
+const header = document.querySelector("header");
+const logo = document.querySelector("#logo");
+const upbar = document.querySelector(".upBar");
+const downbar = document.querySelector(".downBar");
+
+window.addEventListener("scroll", () => {
+  const { scrollY } = window;
+  if (scrollY > 0) {
+    logo.src = "https://www.socar.kr/images/logo-blue.svg";
+    header.classList.add("act");
+    upbar.classList.add("blue");
+    downbar.classList.add("blue");
+    upbar.classList.remove("white");
+    downbar.classList.remove("white");
+  } else {
+    logo.src = "https://www.socar.kr/images/logo-w.svg";
+    header.classList.remove("act");
+    upbar.classList.add("white");
+    downbar.classList.add("white");
+    upbar.classList.remove("blue");
+    downbar.classList.remove("blue");
+  }
+});
+
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+  upbar.classList.toggle("upbarX");
+  downbar.classList.toggle("downbarX");
+});
